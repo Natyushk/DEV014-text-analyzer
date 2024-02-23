8fb03664a729b6c895c1436a4fc04bbef231af43
Natyushk <natamolano_1@hotmail.com>
2024-02-23 15:18:10 -0500

    cambios en el analyzer, en el index

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,21 +1,44 @@
-const analyzer = {  
+const analyzer = {
   getWordCount: (text) => {
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    // Dividir el texto en palabras usando un regex que identifica los espacios en blanco y signos de puntuación como separadores
+    text.trim(); //borrar espacios del inicio y al final del texto
+    const words = text.split(" ")
+    // Retornar la longitud del array de palabras
+    return words.length;
   },
   getCharacterCount: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    // Retornar la longitud del texto, que es el recuento de caracteres
+    text.trim()
+    return text.length;
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    text.trim()
+    const deleteSpace = text.replaceAll(/[,. ]/g, "") //guardo el valor sin espacios rremplazando
+    return deleteSpace.length // retorna el numero de caracteres
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    text.trim(); //borrar espacios del inicio y al final del texto
+    const words = text.split(" ")
+    const deleteSpace = text.replaceAll(" ", "")
+    const average = deleteSpace.length / words.length
+    return average.toFixed(2)
+
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    const number = text.replace(/[^0-9]+/g, "")
+    return number.length
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    const number = text.match(/\d+/g, "")
+    if (number) {
+      const add = number.map(Number).reduce((total, number) => total + number, 0)
+      return add
+    }
+    return 0
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+const textarea = document.querySelector("textarea") //selecciona todo el componente
+const couterlist = document.querySelector("ul").getElementsByTagName("li") 
+textarea.addEventListener("input", () => {
+
+  const text = textarea.value //Trae el valor del componente
+  const wordCount = analyzer.getWordCount(text)
+  const characterCount = analyzer.getCharacterCount(text)
+  const deleteSpace = analyzer.getCharacterCountExcludingSpaces(text)
+  const average = analyzer.getAverageWordLength(text)
+  const number = analyzer.getNumberCount(text)
+  const add = analyzer.getNumberSum(text)
+
+    
+  couterlist[2].textContent = `Palabras: ${wordCount}` //trae las palabras
+  couterlist[0].textContent = `Caracteres: ${characterCount}`
+  couterlist[1].textContent = `Caracteres sin espacio: ${deleteSpace}`
+  couterlist[5].textContent = `Promedio longitud: ${average}`
+  couterlist[3].textContent = `Números: ${number}`
+  couterlist[4].textContent = `Suma Números: ${add}`
+})
