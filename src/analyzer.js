b643fc32ebd066afc9922c55b2ddd37a4381626b
Natyushk <natamolano_1@hotmail.com>
2024-02-26 18:40:39 -0500

    cambios numberCount

--- src/analyzer.js
+++ src/analyzer.js
@@ -30,32 +30,38 @@ const analyzer = {
     return characterCount // retorna el numero de caracteres
   },
   getAverageWordLength: (text) => {
-    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
-    text.trim(); //borrar espacios del inicio y al final del texto
-    const words = text.split(" ")
-    const deleteSpace = text.replaceAll(" ", "")
-    const average = deleteSpace.length / words.length
-    return parseFloat(average.toFixed(2))
+    text = text.trim(); // Eliminar espacios en blanco del principio y final del texto
+    const words = text.split(" "); // Dividir el texto en palabras utilizando el espacio como delimitador
+    let totalLength = 0; // Inicializar una variable para almacenar la suma de las longitudes de las palabras
+    words.forEach(word => { // Iterar sobre cada palabra en el array de palabras
+      totalLength += word.length; // Sumar la longitud de cada palabra a totalLength
+    });
+    const average = totalLength / words.length; // Calcular el promedio dividiendo la suma de las longitudes por el número de palabras
+    return parseFloat(average.toFixed(2)); // Devolver el promedio como un número redondeado a 2 decimales
+
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
     //declarar un campo array/lista
     const numbers = [] //pregunta si es un numero
     for (let i = 0; i < text.length; i++) {
-      const chart = text[i];
-      let number= ""
-      do {
-        console.log (number)
-        number += chart
-        i++
-      } while (["0","1","2","3","4","5","6","7","8","9","."].includes(chart) && i< text.length)
-      //Verifica si es una lista de numeros
-      if (number !== "")
-        numbers.push(number)
-      i++
+      if (["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."].includes(text[i])) { //si encontro un numero
+        //Verifica si es una lista de numeros)
+        let number = ""
+        do {
+          console.log(number)
+          number += text[i]
+          i++
+        } while (["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."].includes(text[i]) && i < text.length)
+        //Verifica si es una lista de numeros
+        if (number !== "") //si el numero es distinto de vacio
+          numbers.push(number)
+        i--
+      }
     }
     return numbers.length
   },
+
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
     const number = text.match(/\d+/g, "")
